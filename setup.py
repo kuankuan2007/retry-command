7bf9e80832a7ba17faac260fbfff7d5dce7e04f0
kuankuan2007 <2163826131@qq.com>
2023-11-20 00:28:18 +0800

    rebuild

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(
     name = 'retryCommand',
-    version = '0.0.1',
+    version = '0.0.2',
     keywords = [],
     description = 'It is a Python library that allows you to retry executing a command multiple times until it succeeds or reaches the maximum number of retries. It provides options to customize the behavior of retries, such as setting a timeout for each execution and specifying the return codes that indicate success.',
     long_description = open("README.md","r",encoding="utf-8").read(),
