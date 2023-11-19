da300f27157ed3946641a69f2398826318b1bebf
kuankuan2007 <2163826131@qq.com>
2023-11-20 00:22:36 +0800

    输出控制

--- retryCommand/__init__.py
+++ retryCommand/__init__.py
@@ -4,6 +4,18 @@ import argparse
 import os
 import rich
 import time
+import io
+
+def ordinalInt(value:int):
+        number=str(value)
+        suffix="th"
+        if (number.endswith("1")):
+            suffix="st"
+        elif (number.endswith("2")):
+            suffix="nd"
+        elif (number.endswith("3")):
+            suffix="rd"
+        return number+suffix
 
 def retryCommand():
     argparser = argparse.ArgumentParser()
@@ -14,38 +26,50 @@ def retryCommand():
     argparser.add_argument('-t', '--time-out', required=False,default=-1,type=int,help="time out to kill process")
     argparser.add_argument('-d','--cwd', required=False,default=os.getcwd(),help="current working directory")
     argparser.add_argument('-n','--success-return-code', required=False,nargs="+",type=int,action="extend",default=[0],help="current working directory")
+    argparser.add_argument('-q','--quiet',action='store_true',default=False,required=False,help="mute more output")
+    argparser.add_argument('--mute',required=False,default=False,action="store_true",help="mute all output")
+    argparser.add_argument('-o','--overwrite-stdin',nargs="*",action="extend",default=[],help="overwrite stdin")
     argparser.add_argument('-c', '--command',nargs=argparse.REMAINDER, required=True,help="command to execute")
     args=argparser.parse_args(sys.argv[1:]).__dict__
     
     if args['time_out']==-1:
         args['time_out']=None
     
+    
+    def output(*arg,**kw):
+        if (not args['quiet'] and not args['mute']):
+            rich.print(*arg,**kw)
+    
     i=0
+    
+    stdout=io.BytesIO() if args['mute'] else None
+    
     while args['max_num_of_retry']==-1 or i<args['max_num_retries']:
         i+=1
-        rich.print(f"[bold blue]try {i} time:[/bold blue]")
+        output(f"[bold blue]the {ordinalInt(i)} attempt:[/bold blue]")
         try:
-            popen=subprocess.Popen(args["command"],cwd=args["cwd"])
+            popen=subprocess.Popen(args["command"],cwd=args["cwd"],stdout=stdout,stderr=stdout)
+            print(stdout)
         except BaseException as e:
-            rich.print(f"[bold red]failed to execute command[/bold red]")
-            rich.print(f"[red]{e.__class__.__name__}:{e}[/red]")
+            output(f"[bold red]failed to execute command[/bold red]")
+            output(f"[red]{e.__class__.__name__}:{e}[/red]")
             if args['no_ignore_process_error']:
-                rich.print("[bold blue]stop after error[/bold blue]")
+                output("[bold blue]stop after error[/bold blue]")
                 break
         else:
             try:
                 popen.wait(args["time_out"])
             except subprocess.TimeoutExpired:
-                rich.print(f"[bold yellow]time out[/bold yellow]")
+                output(f"[bold yellow]time out[/bold yellow]")
                 popen.kill()
-            rich.print(f"[bold green]return code: {popen.returncode}[/bold green]")
+            output(f"[bold green]return code: {popen.returncode}[/bold green]")
             if popen.returncode in args['success_return_code']:
                 if not args['no_stop_after_success']:
-                    rich.print("[bold blue]stop after success[/bold blue]")
+                    output("[bold blue]stop after success[/bold blue]")
                     break
         time.sleep(args["interval"])
-        rich.print("[blue]---------[/blue]")
-    rich.print("[bold blue]done[/bold blue]")
+        output("[blue]---------[/blue]")
+    output("[bold blue]done[/bold blue]")
     
     
 if __name__ == "__main__":
